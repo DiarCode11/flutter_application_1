d6969cd607ab53cc3db20cc0dceb068eabcfe3ee
DiarCode11 <gdsudiartika@gmail.com>
2024-03-25 12:52:59 +0800

    first commit

---
+++ lib/coba.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Halaman1 extends StatefulWidget {
+  const Halaman1({super.key});
+
+  @override
+  State<Halaman1> createState() => _Halaman1State();
+}
+
+class _Halaman1State extends State<Halaman1> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('INI HALAMAN 2');
+  }
+}
+
